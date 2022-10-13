5113282489c1e89348edd33b52ac97f063eb6b45
Teun0n <simon.a.andreasen@gmail.com>
2022-10-13 14:51:23 +0200

    initialize and push

--- src/stack.c
+++ src/stack.c
@@ -2,16 +2,21 @@
 
 void initialize(stack* s){
   //implement initialize here
-  node*
+  s->head=NULL;
 }
 
 void push(int x, stack* s){
     //implement push here
+    node* element=(node*)malloc(sizeof(node));
+    element->next=s->head;
+    element->data=x;
+    s->head=element;
 }
 
 int pop(stack* s){
     // implement pop here
-  return -1;
+    
+  return pop;
 }
 
 bool empty(stack* s)
