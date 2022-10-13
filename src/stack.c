05b739eaf4add7cd6d2030e3784d8dc8e06638d0
Teun0n <simon.a.andreasen@gmail.com>
2022-10-13 15:43:17 +0200

    stack.c needs comments

--- src/stack.c
+++ src/stack.c
@@ -1,4 +1,5 @@
 #include "stack.h"
+#include <assert.h>
 
 void initialize(stack* s){
   //implement initialize here
@@ -15,17 +16,25 @@ void push(int x, stack* s){
 
 int pop(stack* s){
     // implement pop here
-    
+    assert(s->head!=NULL);
+    int pop = s->head->data;
+    s->head=s->head->next;
   return pop;
 }
 
 bool empty(stack* s)
 {
-  //implement empty here
+  if(s->head=NULL)
+  return true; 
+  /*stack is empty if head points at NULL because head points add*/
+  else
   return false;
 }
 
 bool full(stack* s) {
-    //implement full here
+    /*if stack is full false is returned.
+    as no more elements can be pushed.
+    assertion should be added to decide max elements.*/
+    
   return false;
 }
