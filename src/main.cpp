2b2b5d902aad4e357bb5893bb0a6ecb782915257
Teun0n <simon.a.andreasen@gmail.com>
2022-10-13 21:42:53 +0200

    Final

--- src/main.cpp
+++ src/main.cpp
@@ -4,13 +4,38 @@ extern "C"{
 #include <stdio.h>
 #include <math.h>
 }
-
-
-
-
 // Add your test cases for Exercise 1b,c) here
-int main(int argc, char **argv)
+int main(int argc, char **argv) 
 {
-    printf("\n\n%f\n\n",taylor_sine(3.145/2,10));
+    /*program prints output of taylor_sine function and sin function from math.h library.
+    taylor_sine and sin takes x inputs pi/2 through 5*pi/2.
+    this is done three times for n inputs n=1,n=5,n=20 for taylor_sine function.*/
+    double x_input=0;
+    int n_input=1;
+    for(int i=0;i<5;i++)
+    {
+   
+    x_input+=M_PI_2;
+    printf("\ntaylor_sine(%f,%d) = %f   sin(%f) = %f\n",x_input,n_input,taylor_sine(x_input,n_input),x_input,sin(x_input));
+    }
+    x_input=0;
+    n_input=5;
+    for(int j=0;j<5;j++)
+    {
+   
+    x_input+=M_PI_2;
+    printf("\ntaylor_sine(%f,%d) = %f   sin(%f) = %f\n",x_input,n_input,taylor_sine(x_input,n_input),x_input,sin(x_input));
+    }
+    x_input=0;
+    n_input=20;
+    for(int k=0;k<5;k++)
+    {
+    x_input+=M_PI_2;
+    printf("\ntaylor_sine(%f,%d) = %f   sin(%f) = %f\n",x_input,n_input,taylor_sine(x_input,n_input),x_input,sin(x_input));
+    }
+    /* taylor_sine gets closer to math.h sin function, with higher n values.
+    For n=1, only 1 value was close to math.h sin function
+    For n=5, 3 values where close or equal to math.h sin function
+    For n=20 all 5 values were equal to math.h sin function.*/
     return 0;
 }
